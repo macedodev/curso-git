5bf26e14147ceb35695c265ea50927b16acd2fd3
demacedo <aleexmacedoo@gmail.com>
2020-02-20 17:13:59 -0300

    acrescentado menssagem bom dia

--- teste/Program.cs
+++ teste/Program.cs
@@ -4,6 +4,7 @@ namespace teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!!");
             Console.ReadLine();
         }
     }
