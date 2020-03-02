ab7aedb4ecd3f278da09b752329f1adddb660430
demacedo <aleexmacedoo@gmail.com>
2020-03-02 19:13:54 -0300

    Acrescentada a pergunta se esta tudo bem

--- teste/Program.cs
+++ teste/Program.cs
@@ -5,6 +5,7 @@ namespace teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!!");
+            Console.WriteLine("tudo bem");
             Console.ReadLine();
         }
     }
